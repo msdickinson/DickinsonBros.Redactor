954b452684566aee10b21db7130999408b1804c5
Mark Dickinson <marksamdickinson@gmail.com>
2021-01-19 08:25:18 -0700

    Update Test (#13)

--- Tests/DickinsonBros.Redactor.Tests/Configurator/RedactorServiceOptionsConfiguratorTests.cs
+++ Tests/DickinsonBros.Redactor.Tests/Configurator/RedactorServiceOptionsConfiguratorTests.cs
@@ -1,6 +1,6 @@
 ﻿using DickinsonBros.Redactor.Configurator;
 using DickinsonBros.Redactor.Models;
-using DickinsonBros.Test;
+using DickinsonBros.UnitTest;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
